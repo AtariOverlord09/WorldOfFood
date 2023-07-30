10893b81a9bde636f7f911e69a5758ab734611cd
PyPyAtari <antago99088@gmail.com>
2023-07-30 17:56:24 +0300

    fix admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,25 +1,36 @@
 """Регистрация модели пользователя в админке."""
-from django.urls import reverse
-from django.utils.html import format_html
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 
 User = get_user_model()
 
 
-class CustomUserAdmin(admin.ModelAdmin):
-    """Модель пользователя в админке."""
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
 
-    def change_user_link(self, obj):
-        url = reverse('admin:%s_%s_change' % (obj._meta.app_label, obj._meta.model_name), args=[obj.pk])
-        return format_html('<a href="{}">{}</a>', url, obj.username)
 
-    change_user_link.short_description = 'Username'
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = User
+
+
+class CustomUserAdmin(BaseUserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
 
     list_display = (
         'id',
         'username',
+        'first_name',
+        'last_name',
+        'email',
+        'is_staff',
+        'is_active',
     )
+
     list_filter = ('email', 'username')
     search_fields = ('username', 'email', 'first_name', 'last_name')
     list_editable = ('username', 'email', 'first_name', 'last_name')
@@ -28,5 +39,20 @@ class CustomUserAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
     ordering = ('date_joined',)
 
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
 
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+
+
+admin.site.unregister(User)
 admin.site.register(User, CustomUserAdmin)
