4aa78b81aece4836b43245bffdb9094f5a093e93
PyPyAtari <antago99088@gmail.com>
2023-07-30 17:15:06 +0300

    fix admin

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,7 +1,8 @@
 """Регистрация модели пользователя в админке."""
+from django.urls import reverse
+from django.utils.html import format_html
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 User = get_user_model()
 
@@ -9,9 +10,16 @@ User = get_user_model()
 class CustomUserAdmin(admin.ModelAdmin):
     """Модель пользователя в админке."""
 
+    def change_user_link(self, obj):
+        url = reverse('admin:%s_%s_change' % (obj._meta.app_label, obj._meta.model_name), args=[obj.pk])
+        return format_html('<a href="{}">{}</a>', url, obj.username)
+
+    change_user_link.short_description = 'Username'
+
     list_display = (
         'id',
         'username',
+        'change_user_link',
         'first_name',
         'last_name',
         'email',
